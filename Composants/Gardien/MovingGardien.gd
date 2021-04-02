d960596340bef74333933260af0ae370851c306a
Florentin BURGEAT <florentin.ou.2@live.fr>
2021-04-02 20:56:32 +0200

    Correction du déplacement du gardien

--- Composants/Gardien/MovingGardien.gd
+++ Composants/Gardien/MovingGardien.gd
@@ -19,19 +19,18 @@ func _physics_process(delta):
 	var next_direction = get_next_direction(prepos)
 	
 	if next_direction != current_direction:
-		set_directional_properties(current_direction)
 		current_direction = next_direction
-
+		set_directional_properties(current_direction)
 
 func get_next_direction(oldpos: Vector2):
 	var angle :float = rad2deg((get_global_position() - oldpos).angle())
 	if angle >= -135 and angle <= -45:
-		return Direction.RIGHT
-	if angle < -135 or angle > 135:
 		return Direction.UP
-	if angle >= 45 and angle <=135 :
+	if angle < -135 or angle > 135:
 		return Direction.LEFT
-	return Direction.DOWN
+	if angle >= 45 and angle <=135 :
+		return Direction.DOWN
+	return Direction.RIGHT
 
 func set_directional_properties(direction: int) -> void:
 	match direction:
@@ -67,8 +66,8 @@ func animate(direction: String, hflip: bool = false) -> void:
 	$KinematicBody2D/AnimatedSprite.set_offset(Vector2(0,y_offset))
 	
 func place_cone(x:int, y:int, rot: float) -> void:
-	$VisionCone.set_position(Vector2(x, y))
-	$VisionCone.set_rotation_degrees(rot)
+	$KinematicBody2D/VisionCone.set_position(Vector2(x, y))
+	$KinematicBody2D/VisionCone.set_rotation_degrees(rot)
 
 func _on_VisionCone_body_entered(body):
 	if (body.name == "Pingouin"):
